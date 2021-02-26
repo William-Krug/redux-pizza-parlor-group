eaca289d2f4b226ac2f42ae92be306e9ba6f0609
jmbielejeski <jmbielejeski@gmail.com>
2021-02-26 09:39:26 -0700

    added basic styling to admin page

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,5 +1,6 @@
 // VARIABLE NAMES ARE NOT SET.
 // TODO UPDATE VARIABLE NAMES TO MATCH REDUX OBJECT
+import './AdminPage.css'
 
 import { useSelector} from 'react-redux';
 
@@ -8,27 +9,51 @@ function AdminPage() {
   return(
   <>
     <table>
-      <thead>
-        <tr>Name</tr>
-        <tr>Time Order Placer</tr>
-        <tr>Type</tr>
-        <tr>Cost</tr>
-      </thead>
-        <tbody>
+    
+      <tr>
+        <th>Name</th>
+        <th>Time Order Placed</th>
+        <th>Type</th>
+        <th>Cost</th>
+      </tr>
+      <tbody>
+          <tr>
+            <td>Johnny B</td>
+            <td>11:15am</td>
+            <td>Delivery</td>
+            <td>12.99</td>
+          </tr>
+          <tr>
+            <td>Johnny B</td>
+            <td>11:15am</td>
+            <td>Delivery</td>
+            <td>12.99</td>
+          </tr>
+          <tr>
+            <td>Johnny B</td>
+            <td>11:15am</td>
+            <td>Delivery</td>
+            <td>12.99</td>
+          </tr>
+          <tr>
+            <td>Johnny B</td>
+            <td>11:15am</td>
+            <td>Delivery</td>
+            <td>12.99</td>
+          </tr>
           {/*
           {orderList.map(order => {
             return
               <tr>
-           
                 <td>{order.name}</td>
                 <td>{order.time}</td>
                 <td>{order.type}</td>
                 <td>{order.cost}</td>
-          
               </tr>
-          */}
+          
           })}
-        </tbody>
+          */}  
+      </tbody>
     </table>
   </>
   );
