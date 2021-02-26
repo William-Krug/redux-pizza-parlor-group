9fa3d44beec7695220ec52a225cc38546db291aa
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-26 09:25:37 -0600

    redux logger is functional

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App/App';
 
 // Redux store imports
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
